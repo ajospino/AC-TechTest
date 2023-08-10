2e26d2b44e925bd9578fc438f93a4165a1ad8fce
ajospino <ajospinoa@eafit.edu.co>
2023-08-09 22:33:46 -0500

    Added full VPC configuration (IG,Subnet,RT,SG)

--- tf-config/main.tf
+++ tf-config/main.tf
@@ -3,14 +3,89 @@ resource "aws_instance" "ac_technical_test" {
   instance_type = var.instance_type
 }
 
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "ac_tt" {
   allocated_storage    = 10
   db_name              = "mydb"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
+  instance_class       = var.db_instance_type
+  
+  username             = var.db_user
+  manage_master_user_password = true
+  
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
+}
+
+resource "aws_vpc" "ac_tt" {
+  cidr_block = "10.10.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "ac_tt" {
+  vpc_id = aws_vpc.ac_tt.id
+  cidr_block = "10.10.0.0/24"
+}
+
+resource "aws_internet_gateway" "ac_tt" {
+  vpc_id = aws_vpc.ac_tt.id
+}
+
+resource "aws_route_table" "ac_tt" {
+    vpc_id = aws_vpc.ac_tt.id
+
+    route {
+        cidr_block = aws_subnet.ac_tt.cidr_block
+        gateway_id = aws_internet_gateway.ac_tt.id
+    }
+
+    tags = {
+        Name = "main-rt"
+    }
+}
+
+resource "aws_security_group" "ac_tt" {
+        
+    name        = "ac_tt_sg"
+    description = "Allows specific application ports and specific IP addresses for SSH (And TLS)"
+    vpc_id      = aws_vpc.ac_tt.id
+
+    ingress {
+        description      = "SSH from local network ports"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["181.131.210.140/30"]
+    }
+
+    ingress {
+        description      = "Allow TLS access"
+        from_port        = 443
+        to_port          = 443
+        protocol         = "tcp"
+        cidr_blocks      = ["181.131.210.140/30"]
+    }
+
+    ingress {
+        description      = "Allow HTTP access"
+        from_port        = 80
+        to_port          = 80
+        protocol         = "tcp"
+        cidr_blocks      = ["181.131.210.140/30"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "allow_tls"
+    }
 }
--- tf-config/variables.tf
+++ tf-config/variables.tf
@@ -9,3 +9,15 @@ variable "instance_type" {
   type = string
   default = "t2.micro"
 }
+
+variable "db_instance_type" {
+  description = "Instance type for RDS deployment"
+  type = string
+  default = "db.t3.micro"
+}
+
+variable "db_user" {
+    description = "User for Database access"
+    type = string
+    sensitive = true
+}
